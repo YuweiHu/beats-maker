a8f4ecb2b2333f5e32aaa4556210f2240f4f56d3
Yo wei Hu <j821220j821220@gmail.com>
2019-07-10 17:20:34 +0800

    Use keyboard as input

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
-
     pads.forEach((pad, index) => {
-        pad.addEventListener('click', function() {
-            sounds[index].play();
+        console.log(2);
+        window.addEventListener('keydown', function(e) {
+            if (e.keyCode == pad.getAttribute('key')) {
+                sounds[index].currentTime = 0;
+                sounds[index].play();
+            }
         })
     });
 });
