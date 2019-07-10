dbcbc3de5c22a27a001678ddb8539c9fc97f4773
Yo wei Hu <j821220j821220@gmail.com>
2019-07-10 19:36:18 +0800

    Add keydown animation

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
     pads.forEach((pad, index) => {
-        console.log(2);
         window.addEventListener('keydown', function(e) {
             if (e.keyCode == pad.getAttribute('key')) {
                 sounds[index].currentTime = 0;
                 sounds[index].play();
+                pad.style.color = 'white';
+            }
+        })
+        window.addEventListener('keyup', function(e) {
+            if (e.keyCode == pad.getAttribute('key')) {
+                pad.style.color = 'gray';
             }
         })
     });
